c1dc27dea41c19c7c523c5ece8d9eca49e5f2352
rphlmr <raphael@uaerom.com>
2023-11-03 16:44:44 +0100

    📦 NEW: init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Newspaper README
+
+This theme is inspired by Quiet Light.
+
+It is a simple light theme with few colors.
+
+No yellow, green or red tokens (I have a hard time reading them on a light background).
+
+Typescript types are dimmed, like comments.
+
+**Enjoy!**
+
+![](https://raw.githubusercontent.com/rphlmr/vscode-theme-newspaper/master/screenshots/preview.png)
+
---
+++ samples/react.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+type FuncParam = {
+	length: number;
+  }
+  
+  /**
+   * @param {number} length
+   */
+  function func(param: FuncParam) {
+	  var text = 'string';
+	  for (var i = 0; i < param.length; i++) {
+		  text += i;
+	  }
+	  return {
+		  "text": text,
+		  "boolean": false
+	  };
+  }
+  
+  function Component({length}: {length: number}) {
+	return (
+	  <div>
+		{func({length}).text}
+	  </div>
+	);
+  }
+  
+  export function View(){
+	return (
+	  <div className="flex flex-col">
+		<Component length={10} />
+	  </div>
+	);
+  }
